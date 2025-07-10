fa0e1a3b4e4dbf1f2df0242e0330e596939e2753
haashirnawaz9 <153224457+haashirnawaz9@users.noreply.github.com>
2025-07-10 14:07:05 -0400

    Your commit message here

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors')
 
 const corsOptions = {
-    origin: ['http://localhost:5173']
+    origin: ['https://react-express-zd29.vercel.app']
 }
 
 app.use(cors(corsOptions))
